39323bc1336f303f0364029fcd570b9d536c62ea
Akash <akashas@carestack.com>
2025-04-16 19:28:59 +0530

    fixes

---
+++ app/participants/availability/page.tsx
@@ -0,0 +1,9 @@
+import SlotsFinder from "../../components/SlotsFinder";
+
+export default function SlotsPage() {
+  return (
+    <div className="container mx-auto py-6">
+      <SlotsFinder />
+    </div>
+  );
+}
